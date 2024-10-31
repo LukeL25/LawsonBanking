aba4b2009802a4ea969d73f4fb014fea594cad62
LukeL25 <lawsonluke.business@gmail.com>
2024-10-31 13:05:34 -0400

    stuff

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,12 @@ const express = require('express')
 // express app
 const app = express()
 
+// routes
+app.get('/', (req, res) => {
+    res.json({mssg: "Welcome to the app"})
+})
+
 // listen for requests
 app.listen(4000, () => {
-    console.log('listen on port 4000')
+    console.log('listening on port 4000')
 })
